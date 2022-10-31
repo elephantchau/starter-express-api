fdffe91e34901799b54753fa0e88e2a39dcfefeb
elephantchau <elephantchau@gmail.com>
2022-10-31 17:42:42 +0800

    Modified to a simple visitor counter
    To test cyclic file system read write

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    let count = 0;
+
+    if (!fs.existsSync('count.txt')) {
+        fs.writeFileSync('count.txt', count.toString());
+    }
+
+    fs.readFile('count.txt', (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            count = parseInt(data);
+            count++
+            fs.writeFile('count.txt', count.toString(), (err) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res.send("You are visitor number " + count)
+                }
+            })
+        }
+    })
 })
+
 app.listen(process.env.PORT || 3000)
