4017f2d3e0ad52f621e71393af221fdce6a1ff16
elephantchau <elephantchau@gmail.com>
2022-10-31 18:02:39 +0800

    Updated s3 bucket

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 const fs = require('fs')
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
+
+app.all('/', async (req, res) => {
     console.log("Just got a request!")
     let count = 0;
 
-    if (!fs.existsSync('count.txt')) {
-        fs.writeFileSync('count.txt', count.toString());
+
+
+    //check if file exists in s3
+    const params = {
+        Bucket: "cyclic-shrimp-earmuffs-ap-northeast-2",
+        Key: "count.txt"
     }
 
-    fs.readFile('count.txt', (err, data) => {
-        if (err) {
-            console.log(err)
-        } else {
-            count = parseInt(data);
-            count++
-            fs.writeFile('count.txt', count.toString(), (err) => {
-                if (err) {
-                    console.log(err)
-                } else {
-                    res.send("You are visitor number " + count)
-                }
-            })
-        }
-    })
+    try {
+        const data = await s3.headObject(params).promise();
+        console.log("File exists in s3")
+    } catch (err) {
+        console.log("File does not exist in s3");
+        await s3.putObject({
+            Body: count.toString(),
+            Bucket: "cyclic-shrimp-earmuffs-ap-northeast-2",
+            Key: "count.txt",
+        }).promise();
+    }
+
+    
+    count = await s3.getObject({
+        Bucket: "cyclic-shrimp-earmuffs-ap-northeast-2",
+        Key: "count.txt",
+    }).promise()
+
+    count = parseInt(count.Body.toString());
+    count++
+
+    await s3.putObject({
+        Body: count.toString(),
+        Bucket: "cyclic-shrimp-earmuffs-ap-northeast-2",
+        Key: "count.txt",
+    }).promise()
+
+    res.send("count: " + count.toString())
+
 })
 
 app.listen(process.env.PORT || 3000)
